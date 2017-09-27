50001ada379910e64584e437ff2055ee0aaa8eaf
Lesley Stobbe <lesleystobbe@outlook.com>
2017-09-27 14:02:23 +0200

    Added smoothscroll to the landings page

---
+++ Landing Page/assets/js/functions.js
@@ -0,0 +1,34 @@
+// Fire up VueJS
+new Vue({
+    el: '#app',
+    data () {
+        return {
+            title: 'The Akatsuki',
+        }
+    }
+});
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $(".anchor").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
