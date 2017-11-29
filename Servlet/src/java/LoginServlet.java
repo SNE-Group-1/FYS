9d1ded363f76e111cbd121f619af46754bee8814
Lesley Stobbe <lesleystobbe@outlook.com>
2017-11-29 15:59:43 +0100

    bladieblabla

--- Servlet/src/java/LoginServlet.java
+++ Servlet/src/java/LoginServlet.java
@@ -1,14 +1,14 @@
-package net.codejava.servlet;
- 
+
+import java.io.*;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.*;
  
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
  
 @WebServlet("/loginServlet")
 public class LoginServlet extends HttpServlet {
@@ -33,35 +33,10 @@ public class LoginServlet extends HttpServlet {
         htmlResponse += "<h2>Your flight number is: " + flightNumber + "<br/>";      
         htmlResponse += "Your name is: " + name + "</h2>";    
         htmlResponse += "</html>";
-         
+        
         // return response
         out.println(htmlResponse);
-        
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/Servlet", "root", "");
-            
-            if (con != null){
-                System.out.println("Connected to the database");
-            }
- 
-            PreparedStatement ps = con
-                    .prepareStatement("SELECT * FROM flights where flightNumber = " + flightNumber + " AND name = " + name );
- 
-            int i = ps.executeUpdate();
-            if (i > 0){
-                System.out.println("Checked the database");
-            }else{
-                System.out.println("Didnt work");
-            }
-                
-            
- 
-        } catch (Exception e2) {
-            System.out.println(e2);
-        }
-         
+               
     }
  
 }
--- Servlet/src/java/MyServlet.java
+++ Servlet/src/java/MyServlet.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
