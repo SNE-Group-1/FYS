3d0e777d71123d0b3596dae20d4e0392d65d2fb2
Lesley Stobbe <lesleystobbe@outlook.com>
2017-12-20 19:21:01 +0100

    Database connectie gefixt

--- Servlet/src/java/LoginServlet.java
+++ Servlet/src/java/LoginServlet.java
@@ -1,41 +1,71 @@
 
-import java.io.*;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
  
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
  
 @WebServlet("/loginServlet")
 public class LoginServlet extends HttpServlet {
  
+    @Override
     protected void doPost(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
          
         // read form fields
-        String flightNumber = request.getParameter("flightNumber");
+        String ticketNumber = request.getParameter("ticketNumber");
         String name = request.getParameter("name");
          
-        System.out.println("flightNumber: " + flightNumber);
+        System.out.println("ticketNumber: " + ticketNumber);
         System.out.println("name: " + name);
  
         // do some processing here...
-         
+        Reader reader = new Reader(){};
+        
         // get response writer
         PrintWriter out = response.getWriter();
-         
-        // build HTML code
-        String htmlResponse = "<html>";
-        htmlResponse += "<h2>Your flight number is: " + flightNumber + "<br/>";      
-        htmlResponse += "Your name is: " + name + "</h2>";    
-        htmlResponse += "</html>";
         
-        // return response
-        out.println(htmlResponse);
+        try {
+            if(Reader.verify(name, ticketNumber)){
+                System.out.println("Awesome!");
+                // build HTML code
+                String htmlResponse = "<html>";
+                htmlResponse += "<h1>You are logged in</h1>";
+                htmlResponse += "<h2>Your flight number is: " + ticketNumber + "<br/>";      
+                htmlResponse += "Your name is: " + name + "</h2>";    
+                htmlResponse += "</html>";
+                // return response
+                out.println(htmlResponse);
+            }else{
+                System.out.println("Wrong input");
+                String htmlResponse = "<html>";
+                htmlResponse += "<h1>Wrong input</h1>";  
+                htmlResponse += "</html>";
+                // return response
+                out.println(htmlResponse);
+            }
+        } catch (ParseException ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+         
+       
+         
+        
+        
+        
                
     }
  
---
+++ Servlet/src/java/Reader.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+/**
+ *
+ * @author lesley
+ */
+public class Reader {
+    
+        public static boolean verify(String name, String ticketNumber) throws ParseException {
+        JSONParser parser = new JSONParser();
+
+        try {         
+            URL ticketUrl = new URL("http://fys.securidoc.nl:11111/Ticket"); // URL to Parse
+            HttpURLConnection conn = (HttpURLConnection) ticketUrl.openConnection();
+            
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+            
+            String payload = "{\"function\":\"List\", \"teamId\":\"IN105-1\", \"teamKey\":\"9f5549e597bcca8f5c4a190f1b54344a\", \"requestId\":\"1\"}";
+            conn.setRequestProperty("Content-Length", "" + payload.getBytes("UTF-8").length);
+            conn.getOutputStream().write(payload.getBytes("UTF-8"));
+            conn.getOutputStream().flush();
+            
+            InputStreamReader ticketReader = new InputStreamReader(conn.getInputStream());
+            JSONObject rootObject = (JSONObject) parser.parse(ticketReader);
+            
+            if (!rootObject.containsKey("tickets")) {
+                return false;
+            }
+            
+            JSONObject ticketList = (JSONObject) rootObject.get("tickets");
+            
+            // passengerId will not be blank once the right ticket has been found
+            String passengerId = "";
+            for (String ticketId : (Iterable<String>) ticketList.keySet()) {
+                JSONObject ticket = (JSONObject) ticketList.get(ticketId);
+                
+                if (((String)ticket.get("ticketNumber")).equalsIgnoreCase(ticketNumber)) {
+                    passengerId = (String) ticket.get("pid");
+                    break;
+                }
+            }
+            
+            if (passengerId.equals("")) {
+                return false;
+            }
+            
+            URL passengerUrl = new URL("http://fys.securidoc.nl:11111/Passenger");
+            conn = (HttpURLConnection) passengerUrl.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Content-Length", "" + payload.getBytes("UTF-8").length);
+            conn.getOutputStream().write(payload.getBytes("UTF-8"));
+            conn.getOutputStream().flush();
+            
+            InputStreamReader passengerReader = new InputStreamReader(conn.getInputStream());
+            rootObject = (JSONObject) parser.parse(passengerReader);
+            
+            if (!rootObject.containsKey("passengers")) {
+                return false;
+            }
+            
+            JSONObject passengerList = (JSONObject) rootObject.get("passengers");
+            
+            if (!passengerList.containsKey(passengerId)) {
+                return false;
+            }
+            
+            JSONObject passenger = (JSONObject) passengerList.get(passengerId);
+            if (((String) passenger.get("lastName")).equalsIgnoreCase(name)){ 
+                return true;
+            } else {
+                return false;
+            }
+        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
+        }  
+        
+        return false;
+    }   
+    
+}
