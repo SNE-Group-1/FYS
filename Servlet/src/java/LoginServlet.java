2c9d1ab2c03c8c23534adb18e3e61571f91af5aa
Lesley Stobbe <lesleystobbe@outlook.com>
2018-01-10 12:29:52 +0100

    Get IP address

--- Servlet/src/java/LoginServlet.java
+++ Servlet/src/java/LoginServlet.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.HttpURLConnection;
+import java.net.InetAddress;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.servlet.http.HttpServletRequest;
  
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -41,9 +43,24 @@ public class LoginServlet extends HttpServlet {
         try {
             if(Reader.verify(name, ticketNumber)){
                 System.out.println("Awesome!");
+                
+                // Get IP address
+                InetAddress addr = InetAddress.getLocalHost();
+
+                //Getting IPAddress of localhost - getHostAddress return IP Address
+                // in textual format
+                String ipAddress = addr.getHostAddress();
+
+                System.out.println("IP address of localhost from Java Program: " + ipAddress);
+
+                //Hostname
+                String hostname = addr.getHostName();
+                
                 // build HTML code
                 String htmlResponse = "<html>";
                 htmlResponse += "<h1>You are logged in</h1>";
+                htmlResponse += "<p>IP address: " + ipAddress +"</p>";
+                htmlResponse += "<p>Hostname: " + hostname +"</p>";
                 htmlResponse += "<h2>Your flight number is: " + ticketNumber + "<br/>";      
                 htmlResponse += "Your name is: " + name + "</h2>";    
                 htmlResponse += "</html>";
